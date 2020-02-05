70a2dbf95a0536e92192984a991ffebd54c5df41
Jaime Perera <jperera@idealista.com>
2020-02-05 14:38:17 +0100

    Basic maven project structure

---
+++ src/main/java/PicturesManager.java
@@ -0,0 +1,2 @@
+public class PicturesManager {
+}
---
+++ src/test/java/PicturesManagerTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+class PicturesManagerTest {
+
+    @Test
+    public void test() {
+        assertThat(true, is(true));
+    }
+}
